88a3f2e687b30e591bc4ca3d2788ff8d8a09ae00
Roy Mosby <ego.madking@gmail.com>
2021-01-27 21:23:43 -0500

    vestigial to remove

--- lib/controller.rb
+++ lib/controller.rb
@@ -6,61 +6,61 @@ class Controller
     @ingredient_list = []
   end
 
-  def validate_ingredient(ingredient)
-    ingredient = ingredient.downcase
-    if ingredient == "exit"
-      puts "Good bye!"
-    elsif ingredient == "help" || ingredient == "example"
-      @cli.user_help_message("ingredient_#{ingredient}")
-    elsif ingredient == "finished"
-      self.finish_recipe
-    else
-      ingredient_split = ingredient.split(" ")
-      #single word or entry that does not start with a number.
-      if ingredient_split.length <=1 || ingredient_split[0].to_i <= 0
-        @cli.user_help_message('ingredient_reject')
+  # def validate_ingredient(ingredient)
+  #   ingredient = ingredient.downcase
+  #   if ingredient == "exit"
+  #     puts "Good bye!"
+  #   elsif ingredient == "help" || ingredient == "example"
+  #     @cli.user_help_message("ingredient_#{ingredient}")
+  #   elsif ingredient == "finished"
+  #     self.finish_recipe
+  #   else
+  #     ingredient_split = ingredient.split(" ")
+  #     #single word or entry that does not start with a number.
+  #     if ingredient_split.length <=1 || ingredient_split[0].to_i <= 0
+  #       @cli.user_help_message('ingredient_reject')
 
-      #match number or float
-      elsif ingredient_split[0].match?(/\d+$/)
-        @ingredient_list << ingredient
-        @cli.prompt_for_ingredient
+  #     #match number or float
+  #     elsif ingredient_split[0].match?(/\d+$/)
+  #       @ingredient_list << ingredient
+  #       @cli.prompt_for_ingredient
 
-      #match fraction
-      elsif ingredient_split[0].match?(/\d*\/\d*$/)
-        numerator, denominator = ingredient_split[0].split("/")
-        ingredient_split[0] = (numerator.to_f / denominator.to_f).round(2)
-        ingredient = ingredient_split.join(" ")
-        @ingredient_list << ingredient
-        @cli.prompt_for_ingredient
-      else
-        @cli.user_help_message('ingredient_reject')
-      end
-    end
+  #     #match fraction
+  #     elsif ingredient_split[0].match?(/\d*\/\d*$/)
+  #       numerator, denominator = ingredient_split[0].split("/")
+  #       ingredient_split[0] = (numerator.to_f / denominator.to_f).round(2)
+  #       ingredient = ingredient_split.join(" ")
+  #       @ingredient_list << ingredient
+  #       @cli.prompt_for_ingredient
+  #     else
+  #       @cli.user_help_message('ingredient_reject')
+  #     end
+  #   end
     
-  end
+  # end
 
-  def finish_recipe
-    puts "Here is the completed entry: \n\n"
-    pp @ingredient_list
-    puts "Sending recipe to EDAMAM's nutrition analyzer. Please stand by..."
-    self.fetch
-  end
+  # def finish_recipe
+  #   puts "Here is the completed entry: \n\n"
+  #   pp @ingredient_list
+  #   puts "Sending recipe to EDAMAM's nutrition analyzer. Please stand by..."
+  #   self.fetch
+  # end
 
-  def format_req_body
-    @req_body = {
-      "title" => @cli.recipe_name,
-      "yield" => @cli.yield,
-      "ingr" => []
-    }
-    @ingredient_list.each do |i|
-      @req_body["ingr"] << i
-    end
-  end
+  # def format_req_body
+  #   @req_body = {
+  #     "title" => @cli.recipe_name,
+  #     "yield" => @cli.yield,
+  #     "ingr" => []
+  #   }
+  #   @ingredient_list.each do |i|
+  #     @req_body["ingr"] << i
+  #   end
+  # end
 
-  def fetch
-    self.format_req_body
-    @fetcher = Fetcher.init_and_send(self)
-  end
+  # def fetch
+  #   self.format_req_body
+  #   @fetcher = Fetcher.init_and_send(self)
+  # end
 
   def response(resp)
     case resp.code
